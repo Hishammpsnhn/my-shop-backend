fec216482aac6a0aea41b4333f8e538545a6497d
hisham <hishammpsn@gmail.com>
2023-03-31 17:05:28 +0530

    added one product details route

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -31,20 +31,34 @@ export const getProducts = asyncHandler(async (req, res) => {
 // @route   POST /api/products
 // @access  Private/Admin
 export const createProduct = asyncHandler(async (req, res) => {
-    const {name, description,price,user,image,brand,category,countInStock,numReviews} = req.body
+    // const {name, description,price,user,image,brand,category,countInStock,numReviews} = req.body
     const product = new Product({
-      name,
-      price,
-      user,
-      image,
-      brand,
-      category,
-      countInStock,
-      numReviews,
-      description,
+        name: 'Sample name',
+        price: 0,
+        user: "64267c2f45ca07623fda474e",
+        image: 'data:image/jpeg;base64,',
+        brand: 'Sample brand',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description',
     })
   
     const createdProduct = await product.save()
     res.status(201).json(createdProduct)
   })
   
+  
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+export const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+  
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getProducts,createProduct } from "../controllers/productControllers.js";
+import { getProducts, createProduct, getProductById } from "../controllers/productControllers.js";
 
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(createProduct)
+router.route('/:id').get(getProductById)
 
 export default router
