644dd6f6449155b622bf255aaf5a6d68fb101675
hisham <hishammpsn@gmail.com>
2023-04-05 17:34:30 +0530

    add order details

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,6 +1,9 @@
 import expressAsyncHandler from "express-async-handler"
 import Order from "../models/orderModel.js"
 
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Priv
 export const addOrderItems = expressAsyncHandler(async (req, res) => {
     console.log(req.body)
     const {
@@ -19,8 +22,6 @@ export const addOrderItems = expressAsyncHandler(async (req, res) => {
         return
     } else {
         try {
-
-
             const order = new Order({
                 orderItems,
                 user: req.user._id,
@@ -31,13 +32,28 @@ export const addOrderItems = expressAsyncHandler(async (req, res) => {
                 shippingPrice,
                 totalPrice,
             })
-
             const createdOrder = await order.save()
-
-
             res.status(201).json(createdOrder)
+
         } catch (error) {
             console.log(error)
         }
     }
 })
+
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+export const getOrderById = expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'name email'
+    )
+  
+    if (order) {
+      res.json(order)
+    } else {
+      res.status(404)
+      throw new Error('Order not found')
+    }
+  })
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import {
     addOrderItems,
+    getOrderById
 } from '../controllers/orderControllers.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').post(protect,addOrderItems)
+router.route('/:id').get(protect, getOrderById)
 
 export default router
