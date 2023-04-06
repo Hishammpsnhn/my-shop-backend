2c94ab43ac51de3becac3119cb0624b4bbd8cc09
hisham <hishammpsn@gmail.com>
2023-04-06 11:53:27 +0530

    add paypal intengration

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import {
     addOrderItems,
-    getOrderById
+    getOrderById,
+    updateOrderToPaid
 } from '../controllers/orderControllers.js'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -9,5 +10,7 @@ const router = express.Router()
 
 router.route('/').post(protect,addOrderItems)
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').get(protect, updateOrderToPaid)
+
 
 export default router
