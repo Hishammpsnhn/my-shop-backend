aa3a5bd8c9242270591f3b8a392492a9bedfac41
hisham <hishammpsn@gmail.com>
2023-04-07 14:31:31 +0530

    add product review

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getProducts, createProduct, getProductById } from "../controllers/productControllers.js";
-
+import { getProducts, createProduct, getProductById, createProductReview } from "../controllers/productControllers.js";
+import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(createProduct)
 router.route('/:id').get(getProductById)
+router.route('/:id/reviews').post(protect, createProductReview)
 
 export default router
